e51009f198dbbd00c0a7b0a8824cd4a1b9d8fb38
shred03 <mithunyada12345@gmail.com>
2025-09-17 19:47:36 +0530

    health

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ app.use(e.json());
 
 app.use("/api/v3", movieRoutes);
 
+app.get("/api/v3/health", async (req, res) => {
+    res.status(200).send({
+        status: "OK",
+    });
+})
+
 initDatabase().then(() =>{
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
